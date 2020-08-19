7c562aaccc09bb2a95e8463e6ac6d98942f64b6a
Pavelsch00 <58011434+NooBit0@users.noreply.github.com>
2020-08-19 22:50:12 +0300

    Create BD

---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=StudentsDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+            string sqlExpression = "SELECT * FROM Students";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand(sqlExpression, connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows) // если есть данные
+                {
+                    // выводим названия столбцов
+                    Console.WriteLine("{0}\t{1}\t{2}", reader.GetName(0), reader.GetName(1), reader.GetName(2));
+
+                    while (reader.Read()) // построчно считываем данные
+                    {
+                        object id = reader.GetValue(0);
+                        object name = reader.GetValue(1);
+                        object age = reader.GetValue(2);
+                        DateTime ff = DateTime.Parse(reader.GetValue(3).ToString());
+                        Console.WriteLine("{0} \t{1} \t{2} \t{3}", id, name, age, ff.ToShortDateString());
+                    }
+                }
+
+                reader.Close();
+            }
+        }
+    }
+}
